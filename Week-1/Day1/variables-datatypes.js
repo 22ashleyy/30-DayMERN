c09413e82ae056d49c3ce96deeff397fa84ba934
22ashleyy <shlsjane@gmail.com>
2026-03-05 14:16:43 +0300

    learn variable basics

---
+++ Week-1/Day1/variables-datatypes.js
@@ -0,0 +1,18 @@
+//variable = container for storing data values
+
+let x = 100
+console.log(x);
+
+let age = 22;
+console.log(`you are ${age} years old`);
+
+let name = 'ashleys'
+console.log(typeof name);
+
+//booleans
+let online = true;
+console.log(`bro is online: ${online}`);
+
+
+let forSale =  false;
+Console.log(`is this car for sale ${forSale}`);
