f4e23c42f7b516ed26cbecaae8ee706dd442772e
22ashleyy <shlsjane@gmail.com>
2026-03-07 23:18:16 +0300

    learning functions

---
+++ Week-1/Day3/functions.js
@@ -0,0 +1,15 @@
+//functions are a bunch of blocks of things that are repeated in the code
+
+//declaring funtions
+function printMe() {
+  console.log('printing...')
+};
+
+//call function
+ printMe();
+
+function ptintThis(param) {
+  console.log(param);
+}
+
+printThis('tapas');
