6fcb78f4a9074c2d25a8ae4f6155b4f567e9cb79
22ashleyy <shlsjane@gmail.com>
2026-03-18 12:56:17 +0300

    arrays

--- Week-1/Day3/functions.js
+++ Week-1/Day3/functions.js
@@ -1,15 +1,2 @@
 //functions are a bunch of blocks of things that are repeated in the code
 
-//declaring funtions
-function printMe() {
-  console.log('printing...')
-};
-
-//call function
- printMe();
-
-function ptintThis(param) {
-  console.log(param);
-}
-
-printThis('tapas');
